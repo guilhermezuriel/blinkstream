c3c7e9e58f86943a337af02d187fe9f0b2f53801
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-06-12 16:23:57 -0300

    tests - adding category use case with invalid command

--- catalog_admin/application/src/test/java/com/blinkstream/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
+++ catalog_admin/application/src/test/java/com/blinkstream/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,10 +1,14 @@
 package com.blinkstream.catalog.admin.application.category.create;
 
 import com.blinkstream.catalog.admin.domain.category.CategoryGateway;
+import com.blinkstream.catalog.admin.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
@@ -14,7 +18,12 @@ import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
+    @Mock
+    private CategoryGateway categoryGateway;
 
     //1. Caminho perfeito
     //2. Propriedade inválida
@@ -30,19 +39,16 @@ public class CreateCategoryUseCaseTest {
 
         final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
 
-        final CategoryGateway categoryGateway = mock(CategoryGateway.class);
 
-        when(categoryGateway.create(any()))
+        when(this.categoryGateway.create(any()))
                 .thenAnswer(returnsFirstArg());
-
-        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
 
         final var output = useCase.execute(aCommand);
 
         Assertions.assertNotNull(output);
         Assertions.assertNotNull(output.id());
 
-        Mockito.verify(categoryGateway, Mockito.times(1)).create(argThat(aCategory -> {
+        Mockito.verify(this.categoryGateway, Mockito.times(1)).create(argThat(aCategory -> {
             return Objects.equals(aCategory.getName(), expectedName)
                     && Objects.equals(aCategory.getDescription(), expectedDescription)
                     && Objects.equals(aCategory.isActive(), expectedIsActive)
@@ -51,5 +57,22 @@ public class CreateCategoryUseCaseTest {
                     && Objects.isNull(aCategory.getDeletionDate());
                 }
             ));
+    }
+
+
+    @Test
+    public void givenAInvalidCommand_whenCallsCreateCategory_shouldReturnDomainException(){
+        final String expectedMessage = "'Name' should not be null";
+        final var expectedDescription = "Category most popular";
+        final var expectedIsActive = true;
+
+
+        final var aCommand = CreateCategoryCommand.with(null, expectedDescription, expectedIsActive);
+        
+        final var expectedAssertion =  Assertions.assertThrows(DomainException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedMessage, expectedAssertion.getErrors().get(0).message());
+
+        Mockito.verify(this.categoryGateway, Mockito.times(0)).create(any());
     }
 }
